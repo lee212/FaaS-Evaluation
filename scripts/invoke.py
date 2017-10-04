2960fd2ced650556a726e4c9739fdbb6bcf03d5d
Hyungro Lee <hroe.lee@gmail.com>
2017-10-03 21:52:26 -0400

    exception for thread timeout

--- scripts/invoke.py
+++ scripts/invoke.py
@@ -1,5 +1,4 @@
-from multiprocessing.pool import ThreadPool
-import multiprocessing
+from multiprocessing.pool import ThreadPool, TimeoutError
 import urllib
 import time
 from pprint import pprint as pp
@@ -34,7 +33,7 @@ def worker(n):
         r = requests.post(url,data=json.dumps(payload), timeout=timeout_sec)
         res = r.text
     except requests.exceptions.ReadTimeout as e:
-        res = e
+        res = unicode(e, 'utf-8')
     e = time.time() - s
     return (n, res, e)
 
@@ -51,15 +50,17 @@ for i in range(start, end):
     cb = tp.apply_async(worker, (i,))
     cblist.append(cb)
 
+cnt = start
 for i in cblist:
     try:
         n = i.get(timeout_sec)
-    except multiprocessing.TimeoutError as e:
-        n = (i, None, None)
+    except TimeoutError:
+        n = (cnt, None, None)
 
     res[n[0]] = {
             'result': n[1],
             'elapsed_time': n[2]}
+    cnt += 1
 
 tp.close()
 tp.join()

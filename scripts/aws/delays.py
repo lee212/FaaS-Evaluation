8d3955203f4e3950394c0a23b8b17da33f48a248
Hyungro Lee <hroe.lee@gmail.com>
2017-11-05 01:01:49 -0500

    delays for installation and execution

---
+++ scripts/aws/delays.py
@@ -0,0 +1,66 @@
+import json
+import sys
+
+def parse_event(msg):
+    result = {}
+    cut_length = 5
+    mtype = msg[:cut_length]
+    # [INFO]\t2017-10-30T05:39:31.221Z\ta8129aef-bd34-11e7-ac9
+    # 2-6fc3e515585c\t1,1024,29.1802033036,0.383911\n"
+    if mtype == "[INFO]"[:cut_length]:
+        mmtype, mdate, mid, txt = msg.split("\t")
+        result['id'] = mid
+        txt = txt.strip()
+        res = txt.split(",")
+        # loop,mat_n,gflops,elapsed
+        if len(res) == 4:
+            result['gflops'] = float(res[2])
+            result['elapsed'] = float(res[3])
+        # Assume other messages are:
+        # condaruntime does not exist
+        else:
+            result['cold_start'] = True
+    # REPORT RequestId: a8129aef-bd34-11e7-ac92-6fc3e515585c\t
+    # Duration: 15982.64 ms\tBilled Duration: 16000 ms \tMemory Size: 1536 MB\tMax
+    # Memory Used: 547 MB\t\n"
+    elif mtype == "REPORT"[:cut_length]:
+        mhead, duration, rest = msg.split("\t", 2)
+        mid = mhead.split(": ")[1]
+        mseconds = float(duration.split(": ")[1][:-3])
+        result['mseconds'] = mseconds
+        result['id'] = mid
+    # Ignore other type of messages
+    return result
+
+fname = sys.argv[1]
+with open(fname) as f:
+    r = json.load(f)
+
+rdict = {}
+for i in r:
+    mdict = parse_event(i['message'])
+    if mdict:
+        if not mdict['id'] in rdict:
+            rdict[mdict['id']] = mdict
+        else:
+            rdict[mdict['id']].update(mdict)
+
+cslist = []
+tdlist = []
+for k, v in rdict.iteritems():
+    tdiff = 0
+    try:
+        tdiff = v['mseconds'] - (v['elapsed'] * 1000)
+    except:
+        continue
+    if 'cold_start' in v:
+        cslist.append(tdiff)
+    else:
+        if tdiff > 100:
+            print v
+        tdlist.append(tdiff)
+
+with open(fname + ".cslist", "w") as f:
+    json.dump(cslist, f)
+with open(fname + ".tdlist", "w") as f:
+    json.dump(tdlist, f)

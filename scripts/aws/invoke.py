435a3a3007bd125766613ecf5b97b51d16f05362
Hyungro Lee <hroe.lee@gmail.com>
2017-12-07 11:22:53 -0500

    generalized aws lambda invoke function

--- scripts/aws/invoke.py
+++ scripts/aws/invoke.py
@@ -1,33 +1,37 @@
-import botocore.session
-import json
-from multiprocessing.pool import ThreadPool
 import sys
+import json
+import botocore.session
+from multiprocessing.pool import ThreadPool
 
+region = "us-east-2"
+itype = "RequestResponse" #"Event"
 s = botocore.session.get_session()
-c = s.create_client('lambda', region_name='us-east-2')
+c = s.create_client('lambda', region_name=region)
+
 def invoke(x):
-    func_name, cid, loop, mat_n = x
-    r = c.invoke(FunctionName=func_name,
-            Payload=json.dumps({'cid': int(cid),
-                'number_of_loop':int(loop),
-                'number_of_matrix':int(mat_n)}),
-            InvocationType='Event')
+    func_name = x['function_name']
+    r = c.invoke(FunctionName=func_name, Payload=json.dumps(x),
+            InvocationType=itype) 
     return r
 
-def handler(event):
+def handler(event, parallel):
     p = ThreadPool(64)
     res = []
     size = int(event['invoke_size'])
     func_name = event['function_name']
-    loop = event['number_of_loop']
-    mat_n = event['number_of_matrix']
     for i in range(size):
-        argument = (func_name, i, loop, mat_n)
-        res.append(p.apply_async(invoke, args=(argument,)))
+        event['cid'] = i
+        if parallel:
+            res.append(p.apply_async(invoke, args=(event,)))
+        else:
+            res.append(invoke(event))
 
     nres = []
-    for i in res:
-        nres.append(i.get())
+    if parallel:
+        for i in res:
+            nres.append(i.get())
+    else:
+        nres = res
 
     p.close()
     p.join()
@@ -35,20 +39,18 @@ def handler(event):
    
 if __name__ == "__main__":
     if len(sys.argv) < 5:
-        print "invoke_size func_name loop mat_n"
+        print "invoke_size func_name params sequential|concurrent"
         sys.exit()
     isize = sys.argv[1]
     func_name = sys.argv[2]
-    loop = sys.argv[3]
-    mat_n = sys.argv[4]
+    params = json.loads(sys.argv[3])
+    parallel = True if sys.argv[4] == "concurrent" else False
 
     func_names = func_name.split(",")
     res = []
     for func_name in func_names:
-        event = { "function_name": func_name,
-                "invoke_size": isize,
-                "number_of_loop": loop,
-                "number_of_matrix": mat_n }
-        res += (handler(event))
+        params["function_name"] = func_name
+        params["invoke_size"] = isize
+        res += handler(params, parallel)
 
     print res

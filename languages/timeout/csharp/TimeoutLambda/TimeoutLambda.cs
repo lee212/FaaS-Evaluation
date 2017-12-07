c2136c5035db038fab349581d1f08a1aad1f72dd
Hyungro Lee <hroe.lee@gmail.com>
2017-12-07 17:19:17 -0500

    timeout csharp

---
+++ languages/timeout/csharp/TimeoutLambda/TimeoutLambda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+using System.Diagnostics;
+
+namespace TimeoutLambda
+{
+	public class Handler
+	{
+		[LambdaSerializer(typeof(JsonSerializer))]
+			public Result Handle(Request request) {
+				var stopwatch = new Stopwatch();
+				stopwatch.Start();
+				System.Threading.Thread.Sleep(1000);
+				stopwatch.Stop();
+				var elapsed_time = stopwatch.ElapsedMilliseconds;
+				return new Result {
+					ElapsedTime = elapsed_time
+				};
+			}
+	}
+	public class Request
+	{
+		public string Second { get; set; }
+	}
+
+	public class Result
+	{
+		public long ElapsedTime { get; set; }
+	}
+}
+

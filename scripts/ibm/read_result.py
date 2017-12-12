c95720a4d1e07ed6e59de626391dd97360a1072e
Hyungro Lee <hroe.lee@gmail.com>
2017-12-12 12:45:28 -0500

    exec used although dangerous in security reasons

--- scripts/ibm/read_result.py
+++ scripts/ibm/read_result.py
@@ -2,13 +2,13 @@ import sys
 import json
 
 fname = sys.argv[1]
-template = sys.argv[2]
+key =  sys.argv[2]
+template = sys.argv[3]
 
 with open(fname) as f:
     dat = json.load(f)
     for k, v in dat.iteritems():
-        print "{},{},{}".format(v['duration']*1e-3, v['client_info']['elapsed_time'],
-                template)
-                
-
+        exec('val = v' + key)
+        #print "{},{},{}".format(v['duration']*1e-3, v['client_info']['elapsed_time'],
+        exec("print \"" + template + "\".format(val)")
 

2d0152a66d1acba92a308b1f2af6e6eccafd3542
Hyungro Lee <hroe.lee@gmail.com>
2018-01-24 10:57:47 -0500

    updated version of activation read

--- scripts/ibm/get_activation.py
+++ scripts/ibm/get_activation.py
@@ -2,38 +2,51 @@ import sys
 import json
 import requests
 import os
+import argparse
 
-fname = sys.argv[1]
 url = 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/_/activations/'
 auth_string = os.environ['IBM_OPENWHISK_AUTH_STRING']
 
-with open(fname) as f:
-    r = json.load(f)
+def argument_parser():
+    parser = argparse.ArgumentParser("IBM OpenWhisk Activation Results")
+    parser.add_argument("fname", help="filename to obtain activation ids")
+    args = parser.parse_args()
+    return args
 
-actlist = r.keys()
+def collect_activation_ids(fname):
+    with open(fname) as f:
+        r = json.load(f)
 
-tdlist = []
-actdict = {}
-for i in actlist:
-    tdiff = 0
-    # curl -H 'Authorization: Basic
-    # NTNjNmY5ZDctM2JhYy00YjQ1LWI3N2ItNGVhMDMzYzg5YmUwOmNjTWxnaW5GU1VtZENuNGI0aWwxb0RaMVI2RlRNdm9QNUdtaUdlc3A3d25ucDR4QjdKQjZzUVpFQzBkTlZjclI='
-    # -L
-    # 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/_/activations/2cd2e85819ba4a9592e85819ba5a957e'
-    headers = {'Authorization': auth_string}
-    rdata = requests.get(url + i, headers=headers)
-    rdict = rdata.json()
-    mseconds = float(rdict['duration'])
-    # 4443,1,1024,5.86704090704,0.366025
-    cid, loop, mat_n, gflops, elapsed = rdict['response']['result']['msg'].split(",")
-    tdiff = mseconds - (float(elapsed) * 1000)
-    tdlist.append(tdiff)
-    actdict[i] = rdict
+    try:
+        actlist = r.keys()
+    except:
+        actlist = []
+        for i in r:
+            tmp = i['result'].keys()
+            actlist = actlist + list(tmp)
 
-with open(fname + ".tdlist", "w") as f:
-    json.dump(tdlist, f)
+    return actlist
 
-with open(fname + ".activation", "w") as f:
-    json.dump(actdict, f)
+def read_activation_through_rest(actlist):
+    actdict = {}
+    for i in actlist:
+        # curl -H 'Authorization: Basic
+        # NTNjNmY5ZDctM2JhYy00YjQ1LWI3N2ItNGVhMDMzYzg5YmUwOmNjTWxnaW5GU1VtZENuNGI0aWwxb0RaMVI2RlRNdm9QNUdtaUdlc3A3d25ucDR4QjdKQjZzUVpFQzBkTlZjclI='
+        # -L
+        # 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/_/activations/2cd2e85819ba4a9592e85819ba5a957e'
+        headers = {'Authorization': auth_string}
+        rdata = requests.get(url + i, headers=headers)
+        rdict = rdata.json()
+        actdict[i] = rdict
 
+    return actdict
 
+def to_file(fname, data):
+    with open(fname, "w") as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+    args = argument_parser()
+    actids = collect_activation_ids(args.fname)
+    actdict = read_activation_through_rest(actids)
+    to_file("{}.activation".format(args.fname), actdict)

9de77be2d8d20fc74c915987da46342b1170d1b7
Hyungro Lee <hroe.lee@gmail.com>
2018-02-26 22:17:49 -0500

    read timestamp from google stackdriver

---
+++ scripts/google/get_timestamp_from_logs.py
@@ -0,0 +1,46 @@
+import os
+import json
+import argparse
+import datetime
+
+def get_argparse():
+    parser = argparse.ArgumentParser("read timestamp from stackdriver log file")
+    parser.add_argument("fname", help="stackdriver log filename")
+    args = parser.parse_args()
+    return args
+
+def read_file(fname):
+    with open(fname) as f:
+        return json.load(f)
+
+def to_file(fname, data):
+    with open(fname, "w") as f:
+        json.dump(data, f, indent=4)
+
+def read_timestamp(data):
+    res = { "min": datetime.datetime.now(),
+            "max": None,
+            "diff": None }
+    # data is list
+    for i in data:
+        #         "timestamp": "2018-02-26T07:13:46.169000+00:00",
+        t = datetime.datetime.strptime(i['timestamp'][:-6],
+                '%Y-%m-%dT%H:%M:%S.%f')
+        if res['max'] is None:
+            res['max'] = t
+        res['min'] = min(t, res['min'])
+        res['max'] = max(t, res['max'])
+
+    res['diff'] = "{}".format(res['max'] - res['min'])
+    res['min'] = "{}".format(res['min'])
+    res['max'] = "{}".format(res['max'])
+    return res
+
+if __name__ == "__main__":
+    args = get_argparse()
+    rdata = read_file(args.fname)
+    res = read_timestamp(rdata)
+    outfname = "{}.{}.log".format(os.path.basename(__file__).split(".")[0],
+            ".".join(os.path.basename(args.fname).split(".")[:-1]))
+    to_file(outfname, res)
+

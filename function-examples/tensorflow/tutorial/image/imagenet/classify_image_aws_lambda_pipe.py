259d18fb1cc5616c8dce92c92e0bc17ccb83c767
Hyungro Lee <hroe.lee@gmail.com>
2018-03-07 06:21:22 +0000

    updating aws lambda func for tensorflow

--- function-examples/tensorflow/tutorial/image/imagenet/classify_image_aws_lambda_pipe.py
+++ function-examples/tensorflow/tutorial/image/imagenet/classify_image_aws_lambda_pipe.py
@@ -35,24 +35,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import sys
 import boto3
 import argparse
 import os
 import re
-import tempfile
-
+import time
 import numpy as np
-from six.moves import urllib
 import tensorflow as tf
-from multiprocessing import Process, Pipe
+try:
+    from multiprocessing import Pool
+except:
+    pass
 
-s3 = boto3.resource('s3')
+client = boto3.client('s3')
 
 # NOTE: Define the environment variable 'model_bucket_name' in
 # Lambda before running!
-
-MODEL_GRAPH_DEF_PATH = os.path.join(os.sep, 'tmp', 'model.pb')
 
 # Feel free to load these as environment variables through Lambda.
 LABEL_STRINGS_FILENAME = os.path.join(
@@ -128,10 +126,10 @@ class NodeLookup(object):
 def create_graph():
   """Creates a graph from saved GraphDef file and returns a saver."""
   # Creates graph from saved graph_def.pb.
-  with tf.gfile.FastGFile(MODEL_GRAPH_DEF_PATH, 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
+  #with tf.gfile.FastGFile(MODEL_GRAPH_DEF_PATH, 'rb') as f:
+  graph_def = tf.GraphDef()
+  graph_def.ParseFromString(model_body)#f.read())
+  _ = tf.import_graph_def(graph_def, name='')
 
 
 # Creates node ID --> English string lookup.
@@ -139,10 +137,11 @@ print('Loading ID-to-string dict from file...')
 NODE_LOOKUP = NodeLookup()
 
 # This must be called before create_graph().
-print('Downloading Model from S3...')
-s3.Bucket(os.environ['model_bucket_name']).download_file(
-                                                      MODEL_FILENAME,
-                                                      MODEL_GRAPH_DEF_PATH)
+print('Loading Model on S3 to memory...')
+s_model = time.time()
+model_obj = client.get_object(Bucket=os.environ['model_bucket_name'], Key=MODEL_FILENAME)
+model_body = model_obj['Body'].read()
+e_model = time.time()
 
 # Creates graph from saved GraphDef.
 print('Creating TF computation graph...')
@@ -157,9 +156,12 @@ def run_inference_on_image(image):
   Returns:
     Nothing
   """
+  """
   if not tf.gfile.Exists(image):
     tf.logging.fatal('File does not exist %s', image)
   image_data = tf.gfile.FastGFile(image, 'rb').read()
+  """
+  image_data = image
 
   with tf.Session() as sess:
     # Some useful tensors:
@@ -186,42 +188,91 @@ def run_inference_on_image(image):
     return results
     # print('%s (score = %.5f)' % (human_string, score))
 
-def rioim(bucket, key, conn):
-    # load image
-    tmp = tempfile.NamedTemporaryFile()
-    #with open(tmp.name, 'wb') as f:
-    s3.Bucket(bucket).download_file(key, tmp.name)
-    tmp.flush()
-
-    (prediction_label, prediction_prob) = run_inference_on_image(tmp.name)[0]
-
-    conn.send(['(%s, %s)' % (str(prediction_prob), prediction_label)])
-    conn.close()
-
 def lambda_handler(event, context):
-  results = []
-  pcs = []
-  ps = []
-  for record in event['Records']:
-    bucket = record['s3']['bucket']['name']
-    key = record['s3']['object']['key']
+    results = []
+    pcs = []
+    ps = []
+    for record in event['Records']:
+      bucket = record['s3']['bucket']['name']
+      key = record['s3']['object']['key']
 
-    print('Running Deep Learning example using Tensorflow library ...')
-    print('Image to be processed, from: bucket [%s], object key: [%s]' % (bucket, key))
+      print('Running Deep Learning example using Tensorflow library ...')
+      print('Image to be processed, from: bucket [%s], object key: [%s]' % (bucket, key))
 
-    parent_conn, child_conn = Pipe()
-    pcs.append(parent_conn)
-    process = Process(target=rioim, args=(bucket, key, child_conn,))
-    ps.append(process)
-  for p in ps:
-    p.start()
-  print ("start done")
-  for p in ps:
-    p.join()
-  print ("join done")
-  for pc in pcs:
-    results.append(pc.recv())
-  print (results)
-  return results
+      obj = client.get_object(Bucket=bucket, Key=key)
+      body = obj['Body'].read()
 
+      (prediction_label, prediction_prob) = run_inference_on_image(body)[0]
+      results.append( (prediction_label, prediction_prob) )
+    return results
+
+def get_argparse():
+    parser= argparse.ArgumentParser("imagenet example for aws lambda")
+    parser.add_argument("-b", "--bucket")
+    parser.add_argument("-k", "--key")
+    parser.add_argument("-s", "--size", type=int, default=1, help="batch size, e.g. 96 vcpus, 96 batch")
+    args = parser.parse_args()
+    return args
+
+def rioi(args):
+    bucket, key = args
+    s3d_start = time.time()
+    client = boto3.client("s3")
+    obj = client.get_object(Bucket=bucket, Key=key)
+    body = obj['Body'].read()
+    s3d_end = time.time()
+
+    start = time.time()
+    (prediction_label, prediction_prob) = run_inference_on_image(body)[0]
+    end = time.time()
+    res = { "result": (prediction_label, prediction_prob),
+            "elapsed": end - start,
+            "s3d_elapsed": s3d_end - s3d_start }
+    return res
+
+def run_inference_concurrent(bucket, keys, size):
+    event = { "Records": [] }
+    with Pool(processes=size) as p:
+        res = []
+        for key in keys:
+            param = (bucket, key)
+            res.append(p.apply_async(rioi, (param,)))
+
+        nres = []
+        for i in res:
+            nres.append(i.get())
+    return nres
+
+def main(bucket, key, batch_size):
+    if key != "":
+        objs = client.list_objects(Bucket=bucket, Prefix=key)
+    else:
+        objs = client.list_objects(Bucket=bucket)
+    keys = []
+    all_res = []
+    for obj in objs['Contents']:
+        keys.append(obj['Key'])
+        if len(keys) == batch_size:
+            res = run_inference_concurrent(bucket, keys, batch_size)
+            keys = []
+            all_res += res
+    # residue
+    #if cnt == len(objs['Contents']) and loc != 0:
+    if keys:
+        res = run_inference_concurrent(bucket, keys, batch_size)
+        all_res += res
+
+    print (all_res)
+    return all_res
+
+if __name__ == "__main__":
+    args = get_argparse()
+    res = main(args.bucket, args.key, args.size)
+    elapsed_total = 0
+    s3d_elapsed_total = 0
+    for i in res:
+        elapsed_total += i["elapsed"]
+        s3d_elapsed_total += i["s3d_elapsed"]
+    print (("avg elapsed total:{}, avg s3d_elapsed_total:{}, " + \
+        "len:{}").format(elapsed_total/len(res), s3d_elapsed_total/len(res), len(res)))
 

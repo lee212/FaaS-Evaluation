8a14bb2c7fd1bcbe3ed5c626bb6d46eed3a93600
Hyungro Lee <hroe.lee@gmail.com>
2018-03-07 03:46:38 -0500

    update on scan

--- scripts/aws/bigdata_scanner.py
+++ scripts/aws/bigdata_scanner.py
@@ -1,5 +1,6 @@
 import copy
 import argparse
+import invoke
 from logs import to_file
 from bigdata import scanner
 from multiprocessing.pool import ThreadPool
@@ -7,36 +8,50 @@ from bigdata_aggregate_reducer import get_object_keys
 
 bucket = "big-data-benchmark"
 prefix = "pavlo/text/5nodes/rankings/"
+func_name = "bigdata-scan"
 
 def local_handler(event):
     return scanner.lambda_handler(event, None)
 
-def main(x):
+def main(x, local=False):
 
     keys = get_object_keys(bucket, prefix)["keys"]
 
     p = ThreadPool(64)
-    event = { "bucket": "big-data-benchmark",
-         "key": None,
-         "x": x
-        }
+    event = { "function_name": func_name,
+            "bucket": "big-data-benchmark",
+            "key": None,
+            "x": x
+            }
     context = None
     res = []
+    if local:
+        func = local_handler
+    else:
+        func = invoke.lambda_invoke
+
     for key in keys:
         params = copy.deepcopy(event)
         params["key"] = key
-        res.append(p.apply_async(local_handler, args=(params,)))
+        res.append(p.apply_async(func, args=(params,)))
 
     n_res = []
     for i in res:
         n_res.append(i.get())
+
+    # Not saving 'Payload': <botocore.response.StreamingBody object at
+    # 0x7f6a9ab62510>,
+    for j in n_res:
+        del (j['Payload'])
+
     p.close()
     p.join()
     return n_res
 
 def get_argparse():
     parser = argparse.ArgumentParser("run bigdata scan query")
-    parser.add_argument("-x", help="query input for ranking > x")
+    parser.add_argument("-x", type=int, default=1000, help="query input for ranking > x")
+    parser.add_argument("-l", "--local", default=False, action="store_true", help="run locally")
     args = parser.parse_args()
     return args
 
@@ -46,5 +61,5 @@ if __name__ == "__main__":
     q1a, q1b, q1c = 1000, 1000, 10
     x = q1a
     """
-    res = main(args.x)
+    res = main(args.x, args.local)
     to_file("scanner.{}.result".format(args.x), res)
